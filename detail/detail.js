0bc777e7115081c1a1ba50739a9ed0c5e491ea06
Jessica Mosby <jessicamosby@mac.com>
2022-11-14 15:57:53 -0500

    added EventListener in details.js

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,14 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
-// get the id from URL
-// use the id to fetch the dog
-// render and append this dog's details to the container
+// eslint-disable-next-line space-before-function-paren
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+    // get the id from URL
+    const id = params.get('id');
+    // use the id to fetch the dog
+    const dog = await getDog(id);
+    // render and append this dog's details to the container
+    const dogDetailEl = renderDogDetail(dog);
+    dogDetailContainer.append(dogDetailEl);
+});
